c70501ae8ddbe8c49286b73a342dcc45d3f182db
Matheus Reis <matheus.reisluz@gmail.com>
2021-08-04 21:44:03 -0300

    configurando rota

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
+                    
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('coronas', 'CoronaController');
+
